2f3e5123f07d652a70d81e9330ab94fc5ff757a6
E5AT <esatcraft@gmail.com>
2024-10-07 17:26:23 +0300

    minor changes

--- esatCloset/Program.cs
+++ esatCloset/Program.cs
@@ -16,22 +16,30 @@ namespace esatCloset
             List<string> shirt = new List<string>();
             List<string> jacket = new List<string>();
             List<string> trouser = new List<string>();
+            List<string> belt = new List<string>();
+            List<string> shoe = new List<string>();
             List<int> comb = new List<int>();
-            int[] choice = { 0, 1, 1, 1 };
+            int[] choice = { 1, 1, 1, 1, 1, 1 };
             int rand;
+            int answer;
+            
 
             while (true)
             {
-                Console.WriteLine($"[0] T-Shirt: {choice[0]}\n[1] Shirt: {choice[1]}\n[2] Jacket: {choice[2]}\n[3] Trouser: {choice[3]}\n[4] OK");
-                int answer = int.Parse(Console.ReadLine());
-                if (answer == 4) break;
+                Console.WriteLine("Ready presets:\n[0] Schule\n[1] Denim\n[2] Manual");
+                answer = int.Parse(Console.ReadLine());
+                if (answer == 0) choice[0] = 0;
+
+                Console.WriteLine($"[0] T-Shirt: {choice[0]}\n[1] Shirt: {choice[1]}\n[2] Jacket: {choice[2]}\n[3] Trouser: {choice[3]}\n[4] Belt: {choice[4]}\n[5] Shoe: {choice[5]}\n[6] OK");
+                answer = int.Parse(Console.ReadLine());
+                if (answer == 6) break;
                 else if (choice[answer] == 0) choice[answer] = 1;
                 else choice[answer] = 0;
             }
 
             if (choice[0] == 1)
             {
-                StreamReader tR = new StreamReader("E:\\esatCloset\\1tshirt.txt");
+                StreamReader tR = new StreamReader("E:\\esatCloset\\tshirt.txt");
                 string line = tR.ReadLine();
 
                 while (line != null)
@@ -47,7 +55,7 @@ namespace esatCloset
 
             if (choice[1] == 1)
             {
-                StreamReader sR = new StreamReader("E:\\esatCloset\\2shirt.txt");
+                StreamReader sR = new StreamReader("E:\\esatCloset\\shirt.txt");
                 string line = sR.ReadLine();
 
                 while (line != null)
@@ -63,7 +71,7 @@ namespace esatCloset
 
             if (choice[2] == 1)
             {
-                StreamReader jR = new StreamReader("E:\\esatCloset\\3jacket.txt");
+                StreamReader jR = new StreamReader("E:\\esatCloset\\jacket.txt");
                 string line = jR.ReadLine();
 
                 while (line != null)
@@ -79,7 +87,7 @@ namespace esatCloset
 
             if (choice[3] == 1)
             {
-                StreamReader trR = new StreamReader("E:\\esatCloset\\4trouser.txt");
+                StreamReader trR = new StreamReader("E:\\esatCloset\\trouser.txt");
                 string line = trR.ReadLine();
 
                 while (line != null)
@@ -90,6 +98,38 @@ namespace esatCloset
 
                 rand = random.Next(0, trouser.Count - 1);
                 Console.WriteLine(trouser[rand]);
+                comb.Add(rand);
+            } else comb.Add(-1);
+
+            if (choice[4] == 1)
+            {
+                StreamReader bR = new StreamReader("E:\\esatCloset\\belt.txt");
+                string line = bR.ReadLine();
+
+                while (line != null)
+                {
+                    belt.Add(line);
+                    line = bR.ReadLine();
+                }
+
+                rand = random.Next(0, belt.Count - 1);
+                Console.WriteLine(belt[rand]);
+                comb.Add(rand);
+            } else comb.Add(-1);
+
+            if (choice[5] == 1)
+            {
+                StreamReader sR = new StreamReader("E:\\esatCloset\\shoe.txt");
+                string line = sR.ReadLine();
+
+                while (line != null)
+                {
+                    shoe.Add(line);
+                    line = sR.ReadLine();
+                }
+
+                rand = random.Next(0, shoe.Count - 1);
+                Console.WriteLine(shoe[rand]);
                 comb.Add(rand);
             } else comb.Add(-1);
 
